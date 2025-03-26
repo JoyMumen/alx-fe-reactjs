121cecb271925f1a4566b3a5ab1af0ac32de85bb
JoyMumen <joymumengraphic@gmail.com>
2025-03-26 23:19:09 +0100

    update

---
+++ alx-react-app-new/src/components/UserContext.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const UserContext = React.createContext();
+
+export default function UserProvider({ children }) {
+  return <UserContext.Provider>{children}</UserContext.Provider>;
+}
