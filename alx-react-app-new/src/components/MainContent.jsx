7a26d7a1d0774ea4ae691b9c9a8627878b78e340
JoyMumen <joymumengraphic@gmail.com>
2025-03-26 22:04:48 +0100

    update

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+function MainContent() {
+  return (
+    <main>
+      <p
+        style={{
+          fontSize: "48px",
+          fontWeight: "bold",
+          textDecoration: "underline green",
+        }}
+      >
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
