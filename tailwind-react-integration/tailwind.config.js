184369cac72c22c4513112814341ff4ae99a6cc7
JoyMumen <joymumengraphic@gmail.com>
2025-04-12 20:42:28 +0100

    update

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
-  theme: { extend: {} },
-  variants: { extend: {} },
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
+
